40d1f98316f08ff91ad6d18a590fa419a0d90dee
Matt <mattgoh01@gmail.com>
2022-02-08 23:23:32 -0800

    parsing

--- project/Main.hs
+++ project/Main.hs
@@ -2,13 +2,23 @@ module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
+data LispVal = Atom String -- Atom stores string naming atom
+    | List [LispVal]
+    | DottedList [LispVal] LispVal
+    | Number Integer
+    | String String
+    | Bool Bool
+
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
-readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String -- string function returns string
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err -- Left constructor indicates error
+    Right val -> "Found value"           -- Right constructor indicates normal value
 
 main :: IO ()
 main = do
