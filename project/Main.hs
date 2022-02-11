6d489e9fed2c04c9365846076377eb80d3a1f58e
Matt <mattgoh01@gmail.com>
2022-02-10 22:27:27 -0800

    atoms

--- project/Main.hs
+++ project/Main.hs
@@ -9,6 +9,7 @@ data LispVal = Atom String -- Atom stores string naming atom
     | String String
     | Bool Bool
 
+
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
@@ -19,6 +20,24 @@ readExpr :: String -> String -- string function returns string
 readExpr input = case parse (spaces >> symbol) "lisp" input of
     Left err -> "No match: " ++ show err -- Left constructor indicates error
     Right val -> "Found value"           -- Right constructor indicates normal value
+
+parseString :: Parser LispVal
+parseString = do
+    char '"'
+    x <- many (noneOf "\'")
+    char '"'
+    return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom :: do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first:rest
+    return $ case atom of
+               "#t" -> Bool True
+               "$f" -> Bool False
+               _    -> Atom atom
+
 
 main :: IO ()
 main = do
